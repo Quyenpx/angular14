97af1b97d337cd40c9fc7bb0b642987353bf8738
Quyenpx <xuanquyen.ptit@gmail.com>
2022-09-10 10:28:03 +0700

    ngIf

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit {
-
+  public name = 'Quyền Phạm';
+  public age = 25;
   constructor() { }
 
   ngOnInit(): void {
